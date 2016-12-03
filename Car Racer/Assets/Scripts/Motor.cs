1e107f54d6e441b16b86ec98ed85dc48a469cb64
Omri Klein <omrik2007@gmail.com>
2016-12-04 00:56:21 +0200

    first project submision
    A new project is set up with a basic car that drives on the vertical
    axis only.

---
+++ Car Racer/Assets/Scripts/Motor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+
+    public Wheel[] wheels;
+    public float speed = 500f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if(wheels.Length >= 2)
+        {
+            float torque = Input.GetAxis("Vertical") * speed;
+            wheels[wheels.Length - 1].move(torque);
+            wheels[wheels.Length - 2].move(torque);
+
+        }
+    }
+}
---
+++ Car Racer/Assets/Scripts/Wheel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(WheelCollider))]
+public class Wheel : MonoBehaviour {
+
+    WheelCollider wc;
+
+    void Awake()
+    {
+        wc = this.GetComponent<WheelCollider>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void move(float value)
+    {
+        wc.motorTorque = value;
+    }
+}
