21466b9b79086fab56e011420af5513803061493
Omri Klein <omrik2007@gmail.com>
2016-12-05 15:29:05 +0200

    Multiplayer control my player
    network player is added - you control only your player

---
+++ Car Racer/Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+
+    public GameObject myCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (photonView.isMine)
+        {
+            myCamera.SetActive(true);
+            this.GetComponent<Motor>().enabled = true;
+        }
+        else { }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
