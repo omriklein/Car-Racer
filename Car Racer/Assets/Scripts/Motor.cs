ab04e9aef9f705d5890364d413d65a8147e2a8db
Omri Klein <omrik2007@gmail.com>
2016-12-06 19:17:17 +0200

    Multiplayer Working
    Multiplayer is working fine.

--- Car Racer/Assets/Scripts/Motor.cs
+++ Car Racer/Assets/Scripts/Motor.cs
@@ -4,15 +4,50 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class Motor : MonoBehaviour
 {
-
+    /* wheels in order:
+    front left
+    front right
+    back left
+    back right
+    */
     public Wheel[] wheels;
     public float motorPower = 500f;
     public float turnPower = 500f;
+    public Transform centerOfMass;
+
+    /*for the tricks in game
+   public static bool isGruonded = true;
+   public float jumpForce = 100000f;
+   public float trickRotationAngle = 10;
+   */
+
+    private Rigidbody rbody;
+
+    void Awake()
+    {
+        rbody = this.GetComponent<Rigidbody>();
+    }
+
     // Use this for initialization
     void Start()
     {
-
+        rbody.centerOfMass = centerOfMass.localPosition;
     }
+
+    /*for the tricks in game
+      void Update()
+      {
+
+          if (Input.GetKey(KeyCode.Z) && !isGruonded) // rotate left
+          { this.transform.Rotate(Vector3.up, trickRotationAngle); }
+          if (Input.GetKey(KeyCode.X) && !isGruonded) //rotate right
+          { this.transform.Rotate(Vector3.up, -trickRotationAngle); }
+
+          if (Input.GetKeyDown(KeyCode.Space) && isGruonded) // jump
+          { this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce); }
+
+   }
+    */
 
     // Update is called once per frame
     void FixedUpdate()
@@ -32,4 +67,5 @@ public class Motor : MonoBehaviour
 
         }
     }
+
 }
--- Car Racer/Assets/Scripts/NetworkPlayer.cs
+++ Car Racer/Assets/Scripts/NetworkPlayer.cs
@@ -6,6 +6,11 @@ public class NetworkPlayer : Photon.MonoBehaviour
 
     public GameObject myCamera;
 
+    private bool isAlive = true;
+    private Vector3 position;
+    private Quaternion rotation;
+    private float lerpSmoothing = 5f;
+
     // Use this for initialization
     void Start()
     {
@@ -13,13 +18,37 @@ public class NetworkPlayer : Photon.MonoBehaviour
         {
             myCamera.SetActive(true);
             this.GetComponent<Motor>().enabled = true;
+            this.GetComponent<Rigidbody>().useGravity = true;
         }
-        else { }
+        else
+        {
+            StartCoroutine("Alive");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else // stream.isReading
+        {
+            position = (Vector3)stream.ReceiveNext();
+            rotation = (Quaternion)stream.ReceiveNext();
+        }
     }
+
+    IEnumerator Alive()
+    {
+        while (isAlive)
+        {
+            transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * lerpSmoothing);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime *  lerpSmoothing);
+
+            yield return null;
+        }
+    }
+
 }
